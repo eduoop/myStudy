863f6ce6f755fc9bd2801925c0959b52ed3cd762
eduoop <85969484+eduoop@users.noreply.github.com>
2022-05-12 08:35:57 -0300

    6

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,20 +2,21 @@ import Navbar from '../../../src/components/layouts/Navbar'
 import React from 'react'
 import styles from './Home.module.css'
 import {TiMediaPlayOutline} from 'react-icons/ti'
+import Robot from '../../images/study.png'
 
 const Home = () => {
   return (
     <div className={styles.bod}>
       <Navbar />
 
-      <div>
+      <div className={styles.container}>
           <div className={styles.title}>
-              <p >Ola , <br/> caro aluno</p>
+              <p >Ola <span>´</span> , <br/> caro aluno</p>
               <a href=''><TiMediaPlayOutline/> Video introdutivo</a>
           </div>
 
-          <div>
-              <img src='' alt=''/>
+          <div className={styles.image_container}>
+              <img src={Robot} alt='My.study'/>
           </div>
       </div>
     </div>
