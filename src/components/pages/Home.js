82af0bad8453b208f575bfdd901084be255f509a
eduoop <85969484+eduoop@users.noreply.github.com>
2022-05-12 08:07:42 -0300

    05

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,23 @@
 import Navbar from '../../../src/components/layouts/Navbar'
 import React from 'react'
 import styles from './Home.module.css'
+import {TiMediaPlayOutline} from 'react-icons/ti'
 
 const Home = () => {
   return (
     <div className={styles.bod}>
       <Navbar />
+
+      <div>
+          <div className={styles.title}>
+              <p >Ola , <br/> caro aluno</p>
+              <a href=''><TiMediaPlayOutline/> Video introdutivo</a>
+          </div>
+
+          <div>
+              <img src='' alt=''/>
+          </div>
+      </div>
     </div>
   )
 }
