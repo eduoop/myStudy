bbc2bd2eeae6cb888b38bd512cf0a7ec2e4760c5
eduoop <85969484+eduoop@users.noreply.github.com>
2022-05-22 17:14:03 -0300

    pc updated

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,8 +2,8 @@ import Navbar from '../../../src/components/layouts/Navbar'
 import React, { useState } from 'react'
 import styles from './Home.module.css'
 import {TiMediaPlayOutline} from 'react-icons/ti'
-import Robot from '../../images/study.png'
 import Study from '../../images/studys.png'
+import Wave from './background.png'
 
 
 const Home = () => {
@@ -12,18 +12,23 @@ const Home = () => {
 
   return (
     <div className={styles.bod}>
+      <div className={styles.wave_container}>
+        <img src={Wave} alt='wave'/>
+      </div>
+      <div className={styles.image_container}>
+        <img src={Study} alt='My.study'/>
+      </div>
       <Navbar/>
 
-      <div className={styles.container}>
           <div className={styles.title}>
-              <p>Ola <span>´</span> , <br className={styles.break}/> caro aluno!</p>
+              <h1>Ola, <span>´</span> caro aluno!</h1>
+              <p>Seja bem vindo ao seu site de tarefas mais queridinho do brasil</p>
               <h2 className={styles.mobile_description}>Faça login para obter uma experiência <br/> aprimorada!</h2>
-              <a href=''><TiMediaPlayOutline/> Video introdutivo</a>
-          </div>
+              <div className={styles.links_container}>
+                <a href='' className={styles.video_link}><TiMediaPlayOutline/> Video introdutivo</a>
+                <a href='' className={styles.donate_link}>  Doar para o site</a>
+              </div>
 
-          <div className={styles.image_container}>
-              <img src={Robot} alt='My.study'/>
-          </div>
       </div>
 
       <div className={styles.ro_bg}>
@@ -31,6 +36,7 @@ const Home = () => {
         <p>@{year} HorizonTech Todos os direitos reservados</p>
       </div>
 
+      <div className={styles.wave}></div>
     </div>
   )
 }
